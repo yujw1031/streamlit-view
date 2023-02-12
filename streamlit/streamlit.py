98ee379cae1f2d35f0e9867eafba99c1d6ab6ae7
yujw1031 <125179272+yujw1031@users.noreply.github.com>
2023-02-12 16:25:22 +0900

    Add files via upload

---
+++ streamlit/streamlit.py
@@ -0,0 +1,9 @@
+import streamlit as st
+view = [100,150,30]
+st.write('# Youtube view') # 제일큰 폰트 - #
+st.write(' ## raw') # 두번쨰로 큰 폰트 = ##
+st.write(' ## bar chart')
+language = "안녕하세요"
+language
+
+st.bar_chart(view)
